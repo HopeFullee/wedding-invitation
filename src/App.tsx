f0df23af4fa944d88f7e2c9d1d4ddc3578b11d7e
HopeFullee <john_lee0607@naver.com>
2026-07-11 01:56:28 +0900

    chore: testing preview cleanup 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="">
-      <h1>testing previous preview deploy removal on "PR CLOSE" 5</h1>
+      <h1>testing previous preview deploy removal on "PR CLOSE" 10</h1>
     </div>
   );
 }
