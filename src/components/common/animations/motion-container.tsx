debdfffdadc64555bd962355ad9435a7c9cb4ba8
HopeFullee <john_lee0607@naver.com>
2026-08-10 02:48:32 +0900

    chore: added new animation Variant

--- src/components/common/animations/motion-container.tsx
+++ src/components/common/animations/motion-container.tsx
@@ -60,6 +60,21 @@ export const FADE_IN_VARIANT: Variants = {
   },
 };
 
+export const FADE_IN_UP_VARIANT: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeInOut",
+    },
+  },
+};
+
 export const BLUR_OUT_VARIANT: Variants = {
   hidden: {
     y: 16,
