4cba90aedbf23816492b5b06180e48b48b4cc976
HopeFullee <john_lee0607@naver.com>
2026-08-11 16:33:48 +0900

    chore: changed DOM Viewport height to true-viewport height

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,6 +3,10 @@ interface Props {
 }
 
 export const Layout = ({ children }: Props) => {
+  // 최초 1회 "모바일 URL-bar 크기를 제외한" true-viewport height 크기를 계산 및 DOM 객체에 주입 (초기화)
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
   return (
     <main className="w-full mx-auto bg-[#F6EEE5] min-h-screen max-w-480">
       {children}
